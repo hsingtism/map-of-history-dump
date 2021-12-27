a9c0b372ca37a6c3dbcdf7e1735690073ad6ccf2
Hsingtism <hsing@flyingbat.net>
2021-12-26 23:33:19 -0500

    avoid lookup if no geolocation data

--- get-coord-from-dump.js
+++ get-coord-from-dump.js
@@ -8,12 +8,16 @@ var lineReader = require('readline').createInterface({
 var ln = 0
 var ve = 0
 var bst = ''
-var out
 
-const cNeeded = JSON.parse(fs.readFileSync('neededCoord.json','utf-8'))
+const cNeeded = JSON.parse(fs.readFileSync('neededCoord.json', 'utf-8'))
 
 setInterval(() => {
-    console.log(Date.now(), ln, ve, JSON.stringify(out))
+    console.log(Date.now(), ln, ve)
+    console.log('WRITTEN')
+    fs.appendFile('coordinateLookup.json', bst, (err) => {
+        console.log(err)
+    })
+    bst = ''
 }, 5000);
 
 lineReader.on('line', (line) => {
@@ -26,32 +30,33 @@ lineReader.on('line', (line) => {
         bst = ''
         return
     }
+    processLine(line)
+});
+
+function processLine(line) {
     const obj = JSON.parse(line.substring(0, line.length - 1)) //excludes delimiting comma
-    out = {
+    var out = {
         i: '',
         lat: false,
         lon: false,
         per: false,
     }
-    if(cNeeded.indexOf(obj.id) == -1) return null
+    if (!obj.claims.P625) return
+    if (cNeeded.indexOf(obj.id) == -1) return null
     try {
         out.i = obj.id
         out.lat = obj.claims['P625'][0]['mainsnak']['datavalue']['value']['latitude']
         out.lon = obj.claims['P625'][0]['mainsnak']['datavalue']['value']['longitude']
-        out.per = obj.claims['P625'][0]['mainsnak']['datavalue']['value']['precision']
     } catch (err) {
         return null
     }
-    ve++
-    if (ve % 100) {
-        bst += JSON.stringify(out)
-        bst += ',\n'
-        return null
+    try {
+        out.per = obj.claims['P625'][0]['mainsnak']['datavalue']['value']['precision']
+    } catch (err) {
+        console.log('no percision')
+        out.per = -32768 //arbitrary
     }
-    console.log('WRITTEN')
-    fs.appendFile('coordinateLookup.json', bst, (err) => {
-        console.log(err)
-    })
-    bst = ''
+    ve++
+    bst += JSON.stringify(out) + '\n'
     return null
-});
+}
