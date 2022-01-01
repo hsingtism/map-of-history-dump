3344ba06acea11855c3ac3390634bfc41fb4e127
Hsingtism <hsing@flyingbat.net>
2021-12-31 23:36:51 -0500

    completed

--- plot.py
+++ plot.py
@@ -2,10 +2,10 @@ import datashader as ds, pandas as pd
 from datashader.utils import export_image
 from colorcet import fire
 
-df = pd.read_csv('E:\\map-of-history\\ready.csv')
+df = pd.read_csv('E:\\map-of-history\\final-dat\\2021.csv')
 
 # cvs = ds.Canvas(plot_width=700, plot_height=700)
-cvs = ds.Canvas(plot_width=1920, plot_height=1080)
+cvs = ds.Canvas(plot_width=1024, plot_height=512)
 agg = cvs.points(df, "lon", "lat")
 img = ds.tf.shade(agg, cmap=fire, how='eq_hist')
-export_image(img, "out", background="black", export_path="E:\\map-of-history\\images")
+export_image(img, "2021", background="black", export_path="E:\\map-of-history\\images")
